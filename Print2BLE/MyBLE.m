edaae4c33e4770c047339126ae61fd3082b360c2
zirafa <zirafa@gmail.com>
2024-12-19 02:13:07 -0600

    Fix typo, add template

--- Print2BLE/MyBLE.m
+++ Print2BLE/MyBLE.m
@@ -51,7 +51,7 @@ const unsigned char ucMirror[256]=
 
 - (uint8_t)findPrinter: (const char *) name
 {
-    const char *szTypes[] = {"MTP-II", "MTP-2", "MTP-3", "MTP-3F", "PeriPage+", "PeriPage_", "GT01", "GT02", "GB01", "GB02", "YHK-54A8", "MX06", "D110-E8", "M02S" NULL};
+    const char *szTypes[] = {"MTP-II", "MTP-2", "MTP-3", "MTP-3F", "PeriPage+", "PeriPage_", "GT01", "GT02", "GB01", "GB02", "YHK-54A8", "MX06", "D110-E8", "M02S", NULL};
     const uint8_t ucTypes[] = {PRINTER_MTP2, PRINTER_MTP2, PRINTER_MTP3, PRINTER_MTP3, PRINTER_PERIPAGEPLUS, PRINTER_PERIPAGE, PRINTER_CAT, PRINTER_CAT, PRINTER_CAT, PRINTER_CAT, PRINTER_PANDA, PRINTER_CAT, PRINTER_PANDA, PRINTER_PERIPAGEPLUS};
     char szTemp[32];
     uint8_t ucType = 255; // invalid
